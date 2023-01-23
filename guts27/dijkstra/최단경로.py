ef30ee25e41efc3a7db261eb467435731d466801
guts27 <52551159+guts27@users.noreply.github.com>
2023-01-23 18:58:10 +0900

    230123_1.5_BOJ_1753_guts27

---
+++ guts27/dijkstra/최단경로.py
@@ -0,0 +1,33 @@
+import heapq
+import sys
+input = sys.stdin.readline
+V,E = map(int,input().split())
+start = int(input())
+graph = [[] for i in range(V + 1)]
+for _ in range(E):
+    i,j,w = map(int,input().split())
+    graph[i].append((j,w))
+distance = [1e9]*(V+1)
+
+def dijkstra (start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q: 
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+dijkstra(start)
+
+for i in range(1, V + 1):
+    if distance[i] == 1e9:
+        print("INF")
+    else:
+        print(distance[i])
