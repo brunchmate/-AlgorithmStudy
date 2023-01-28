3f173561389a929e4021bf07241c263958ec3be7
guts27 <52551159+guts27@users.noreply.github.com>
2023-01-28 16:47:30 +0900

    230128_1_BOJ_1507_guts27

--- guts27/FloydWarshall/궁금한민호.py
+++ guts27/FloydWarshall/궁금한민호.py
@@ -1,22 +1,30 @@
+def floyd():
+  
+    for k in range(n):
+        for j in range(n):
+            for i in range(n):
+                if i == k or j == k or i==j:
+                    continue
+
+                if graph[i][j] == graph[i][k] + graph[k][i]:
+                    graph[i][j] = 0
+                  
+                elif graph[i][j] > graph[i][k]+graph[k][j]:
+                    return False
+                
+    res = 0
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j] > 0:
+                res += graph[i][j]
+
+    return res
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
-for k in range(n):
-    for j in range(n):
-        for i in range(n):
-            if i == k or j == k:
-                    continue
-            if graph[i][j] > graph[i][k]+[k][j]:
-                print(-1)
-            if graph[i][j] == graph[i][k] + graph[k][i]:
-                graph[i][j] = 0
-            
-result = 0
-for i in range(n):
-    for j in range(n):
-        result += graph[i][j]
+result = floyd()
 
-if result == 0:
+if not result:
     print(-1)
 else:
     print(result)
