e0ea7f6eb3aed7209cc2da7f59098868e4b39f7b
guts27 <52551159+guts27@users.noreply.github.com>
2023-02-01 20:45:09 +0900

    230201_1_BOJ_1931_guts27

---
+++ guts27/Greedy/에너지드링크.py
@@ -0,0 +1,12 @@
+
+n = int(input())
+drinks = list(map(int,input().split()))
+max_num = max(drinks)
+drinks.remove(max_num)
+print(sum(drinks)/2 + max_num)
+
+drinks.sort()
+
+total = drinks[-1]
+for i in range(N-1):
+    total += drinks[i]/2
