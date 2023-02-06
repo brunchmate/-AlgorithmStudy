a35b214ccbba17aadb370bb072f8352397f59855
guts27 <52551159+guts27@users.noreply.github.com>
2023-02-06 19:34:00 +0900

    230206_1_KaKao_튜플_guts27

---
+++ guts27/2019 카카오 개발자 겨울 인턴십/튜플.py
@@ -0,0 +1,20 @@
+def solution(s):
+    answer = []
+    num = []
+    s= s[1:-1]
+    start = 0
+    end = 0
+    for i in range(len(s)):
+        if s[i] == '{':
+            start = i
+            continue
+        if s[i] == '}':
+            end = i
+            num.append(list(map(int, s[start+1:end].split(','))))
+    #길이 순으로 정렬
+    num.sort(key=len)
+    for n in num:
+        for z in n:
+            if z not in answer:
+                answer.append(z)
+    return answer
