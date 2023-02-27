5d8557104b912a02068dbebe39325c6946f7c781
maplejh <cjh0313c@gmail.com>
2023-02-27 18:20:04 +0900

    230227_1_2018 KAKAO BLIND RECRUITMENT [1차]_캐시_maplejh

---
+++ maplejh/Programmers/캐시.py
@@ -0,0 +1,17 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/17680
+from collections import deque
+
+
+def solution(cacheSize, cities):
+    answer = 0
+    cache = deque(maxlen=cacheSize)
+    for city in cities:
+        city = city.lower()
+        if city in cache:
+            cache.remove(city)
+            cache.append(city)
+            answer += 1
+        else:
+            cache.append(city)
+            answer += 5
+    return answer
