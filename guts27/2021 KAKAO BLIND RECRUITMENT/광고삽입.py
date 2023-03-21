fda70a83dfa2ec70fe5d37ba8c081b12c05404d0
guts27 <52551159+guts27@users.noreply.github.com>
2023-03-21 22:31:37 +0900

    230321_1_2021 KAKAO BLIND RECRUITMENT_guts27

--- guts27/2021 KAKAO BLIND RECRUITMENT/광고삽입.py
+++ guts27/2021 KAKAO BLIND RECRUITMENT/광고삽입.py
@@ -1,47 +1,34 @@
+def time2int(time):
+    time = list(map(int,time.split(':')))
+    return time[0]*3600 + time[1]*60 + time[2]
+def int2time(num):
+    q,r = divmod(num,3600)
+    temp = str(q)
+    q,r = divmod(r,60)
+    return temp.zfill(2)+":"+str(q).zfill(2)+":" +str(r).zfill(2)
+    return
 def solution(play_time, adv_time, logs):
-    play_time = str_to_int(play_time)        # 1
-    adv_time = str_to_int(adv_time)               
-    all_time = [0 for i in range(play_time + 1)]
-
-    for l in logs:                           # 2
-        start, end = l.split('-')
-        start = str_to_int(start)
-        end = str_to_int(end)
-        all_time[start] += 1
-        all_time[end] -= 1
-
-    for i in range(1, len(all_time)):       # 3
-        all_time[i] = all_time[i] + all_time[i - 1]
-
-    for i in range(1, len(all_time)):       # 4
-        all_time[i] = all_time[i] + all_time[i - 1]
-
-    most_view = 0                           # 5
-    max_time = 0                          
-    for i in range(adv_time - 1, play_time):
-        if i >= adv_time:
-            if most_view < all_time[i] - all_time[i - adv_time]:
-                most_view = all_time[i] - all_time[i - adv_time]
-                max_time = i - adv_time + 1
-        else:
-            if most_view < all_time[i]:
-                most_view = all_time[i]
-                max_time = i - adv_time + 1
-
-    return int_to_str(max_time)
-
-
-def str_to_int(time):
-    h, m, s = time.split(':')
-    return int(h) * 3600 + int(m) * 60 + int(s)
-
-
-def int_to_str(time):
-    h = time // 3600
-    h = '0' + str(h) if h < 10 else str(h)
-    time = time % 3600
-    m = time // 60
-    m = '0' + str(m) if m < 10 else str(m)
-    time = time % 60
-    s = '0' + str(time) if time < 10 else str(time)
-    return h + ':' + m + ':' + s
+    answer = ''
+    play_time = time2int(play_time)
+    adv_time = time2int(adv_time)
+    timelines = [0] * (play_time + 1)
+    maxnum = -1
+    
+    for log in logs:
+        log = log.split('-')
+        start = time2int(log[0])
+        end = time2int(log[1])
+        timelines[start] += 1
+        timelines[end] -= 1
+        
+    for i in range(1,play_time+1):
+        timelines[i] = timelines[i] + timelines[i-1]
+    for i in range(1,play_time+1):
+        timelines[i] = timelines[i] + timelines[i-1]
+        
+    for i in range(adv_time-1,play_time):
+        num = timelines[i] - timelines[i-adv_time]
+        if num > maxnum:
+            answer = i-adv_time +1
+            maxnum = num
+    return int2time(answer)
