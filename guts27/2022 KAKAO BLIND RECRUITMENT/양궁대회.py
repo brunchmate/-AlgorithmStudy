52346fd415d08fb1f46ed5ee668e1d71b8c6df4e
guts27 <52551159+guts27@users.noreply.github.com>
2023-03-29 22:50:23 +0900

    230329_0_2022 Kakao blind recruitment_guts27

--- guts27/2022 KAKAO BLIND RECRUITMENT/양궁대회.py
+++ guts27/2022 KAKAO BLIND RECRUITMENT/양궁대회.py
@@ -1,23 +1,32 @@
 def dfs(v, visited,count,info):
     global answer
-    global score_a
-    if v < 0:
-        score_l = score(visited)
-        if score_a < score_l and  answer[0]<= score_l:
-            print(score_l,visited)
+    arr = [10,9,8,7,6,5,4,3,2,1,0]
+    
+    if count == 0 or v == -1:     
+        score_l = 0
+        score_a = 0
+        for i in range(10):
+            if info[i] == 0 and visited[i]== 0:
+                continue
+            if info[i]>=visited[i]:
+                score_a  =  score_a + arr[i]
+            else:
+                score_l = score_l + arr[i]
+
+        if score_a < score_l and  answer[0] < score_l:
+            if count != 0:
+                visited[-1] = visited[-1]+ count
             answer = [score_l,visited[:]]
         return
-            
+    
+    dfs(v-1, visited[:],count,info)
+    
     if count > info[v]:
         visited[v] = info[v]+1
-        count = count-(info[v]+1)
-        dfs(v-1, visited,count,info)
+        dfs(v-1, visited[:],count-(info[v]+1),info)
         visited[v] = 0
-        count = count+(info[v]+1)
     
-    dfs(v-1, visited,count,info)
-    
-    return visited
+        
 
 def score(visited):
     num = 0
@@ -26,14 +35,14 @@ def score(visited):
         if visited[i] != 0:
             num += arr[i]
     return num
+
 def solution(n, info):
-    global answer,score_a
+    global answer
     answer = [0,[0]]
     num = 0
-    score_a = score(info)
     visited = [0 for _ in range(11)]
     dfs(10, visited,n,info)
         
-    if answer[0] == 0:
+    if len(answer[1]) == 1:
         return [-1]
     return answer[1]
