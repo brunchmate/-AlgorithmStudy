fb7c170b1125065a355b71064c5f22695cddb2ad
guts27 <52551159+guts27@users.noreply.github.com>
2023-04-03 15:20:21 +0900

    230403_1_ 2022 KAKAO TECH INTERNSHIP_guts27

---
+++ guts27/2022 KAKAO TECH INTERNSHIP/코딩 테.스트 공부.py
@@ -0,0 +1,26 @@
+def solution(alp, cop, problems):
+    answer = 0
+    max_alp, max_cop = [0, 0]
+    for problem in problems:
+        max_alp = max(max_alp, problem[0])
+        max_cop = max(max_cop, problem[1])
+    dp = [[1e9] * (max_cop+1) for _ in range(max_alp+1)]
+        
+    alp = min(alp, max_alp)  
+    cop = min(cop, max_cop)
+    
+    dp[alp][cop] = 0
+    
+    for i in range(alp, max_alp+1):
+        for j in range(cop, max_cop+1):
+            if i < max_alp:
+                dp[i+1][j] = min(dp[i+1][j], dp[i][j] + 1)
+            if j < max_cop:
+                dp[i][j+1] = min(dp[i][j+1], dp[i][j] + 1)
+            
+            for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
+                if i >= alp_req and j >= cop_req:
+                    new_alp = min(i+alp_rwd, max_alp)  
+                    new_cop = min(j+cop_rwd, max_cop)
+                    dp[new_alp][new_cop] = min(dp[new_alp][new_cop], dp[i][j] + cost)
+    return dp[max_alp][max_cop]
