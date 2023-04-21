64ee4a7f46b194937306c24d04fbbcd389411ddb
guts27 <52551159+guts27@users.noreply.github.com>
2023-04-21 21:45:32 +0900

    230421_1_백준1260_guts27

--- guts27/DFS/DFS와 BFS.py
+++ guts27/DFS/DFS와 BFS.py
@@ -1,42 +1,39 @@
 from collections import deque
 
-def dfs(v,visited,graph):
-    index=1e9
-    d_answer.append(v+1)
-    if len(d_answer) == len(graph):
-        return
-    visited[v] = True
-    for b in graph[v]:
-        if visited[b] == False and index > b:
-            index = b
-    dfs(index,visited,graph)
+def dfs(a):
+    print(a, end = " ")
+    visited[a] = True
+    for b in graph[a]:
+        if visited[b] == False:
+            dfs(b)
     
-def bfs(v,visited,graph):
+def bfs(a):
     queue = deque()
-    queue.append(v)
-    visited[v] = True
+    queue.append(a)
+    visited[a] = True
     while queue:
         a = queue.popleft()
-        b_answer.append(a+1)
+        print(a, end= " ")
         for b in graph[a]:
             if visited[b] == False:
                 visited[b] = True
                 queue.append(b)
-
-global d_answer, b_answer
-d_answer = []
-b_answer = []
+                
 n,m,v = map(int,input().split())
-graph = [[] for _ in range(n)]
-visited = [False for i in range(n)]
+graph = [[] for _ in range(n+1)]
+visited = [False]*(n+1)
 for _ in range(m):
     a,b = map(int,input().split())
-    graph[a-1].append(b-1)
-    graph[b-1].append(a-1)
-for i in range(n):
-    graph[i].sort()
-dfs(v-1,visited,graph)
-visited = [False for i in range(n)]
-bfs(v-1,visited,graph)
-print(*d_answer)
-print(*b_answer)
+    graph[a].append(b)
+    graph[b].append(a)
+    
+#내가 틀린 이유!!!!!!!
+#for i in range(n):
+#    graph[i].sort()
+# 정렬
+for i in graph:
+    i.sort()
+dfs(v)
+print()
+visited = [False]*(n+1)
+bfs(v)
