06b14ce34845a3098525e890174f6742e176d2fe
maplejh <cjh0313c@gmail.com>
2023-04-23 15:24:10 +0900

    230423_1_프로그래머스_징검다리 건너기_maplejh

--- maplejh/Programmers/징검다리 건너기.py
+++ maplejh/Programmers/징검다리 건너기.py
@@ -21,3 +21,24 @@ def solution(stones, k):
 
 
 # 이분 탐색으로도 가능
+def solution(stones, k):
+    l = 0
+    r = max(stones)
+    while l <= r:
+        mid = (l + r) // 2
+        tmp = 0
+        flag = 1
+        for s in stones:
+            s -= mid
+            if s <= 0:
+                tmp += 1
+                if tmp == k:
+                    flag = 0
+                    break
+            else:
+                tmp = 0
+        if flag:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return mid
