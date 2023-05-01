41ad89df560036dda993811ceff5ccb3b429ce8f
guts27 <52551159+guts27@users.noreply.github.com>
2023-05-01 14:57:34 +0900

    0501_1_Programmers_정수삼각형_guts27

---
+++ guts27/DP/정수 삼각형.py
@@ -0,0 +1,7 @@
+def solution(triangle):
+    answer = 0
+    
+    for i in range(len(triangle)-2, -1,-1):
+        for j in range(len(triangle[i])):
+            triangle[i][j] = triangle[i][j] + max(triangle[i+1][j],triangle[i+1][j+1])
+    return triangle[0][0]
