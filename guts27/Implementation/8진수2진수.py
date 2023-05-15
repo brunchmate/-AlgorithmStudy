a266b3da8fbed337dfed1c90c7bf2d10c94edd9a
guts27 <52551159+guts27@users.noreply.github.com>
2023-05-15 22:33:21 +0900

    0515_1_BOJ_1212_guts27

---
+++ guts27/Implementation/8진수2진수.py
@@ -0,0 +1,13 @@
+print(bin(int(input(),8))[2:])
+
+# dic = {'0':'000', '1':'001','2':'010','3':'011','4':'100','5':'101','6':'110','7':'111'}
+
+# num = input()
+# result = ""
+# for n in num:
+#     result += dic[n]
+# if int(result) == 0:
+#     print(0)
+# else:
+#     print(str(int(result)))
+    
